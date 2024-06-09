fda232fd83ec8c455ba9035c3573d047f65de55c
Sonny Peters <167081935+odeepeters@users.noreply.github.com>
2024-06-09 23:23:48 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import ('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'pacifico': ["'Pacifico'", 'Static'],
+      },
+    },
+  },
+  plugins: [],
+}
+
